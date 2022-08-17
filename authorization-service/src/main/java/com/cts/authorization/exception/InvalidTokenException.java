8fe276ab2cf7cedbb56536f19fd89732bda4f36c
pawar <vishalpawar12499@gmail.com>
2022-08-17 11:32:21 +0530

    Authorization service

---
+++ authorization-service/src/main/java/com/cts/authorization/exception/InvalidTokenException.java
@@ -0,0 +1,14 @@
+package com.cts.authorization.exception;
+
+/**
+ * Class to handle Invalid token given by the JWT
+ *
+ */
+public class InvalidTokenException extends RuntimeException {
+
+	private static final long serialVersionUID = -3700741193887093791L;
+
+	public InvalidTokenException(String message) {
+		super(message);
+	}
+}
